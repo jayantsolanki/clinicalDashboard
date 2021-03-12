06f985794dcc785ac387e7c5d0552ebfbbc65aac
Vinay <vinayprakashanalytics.808@gmail.com>
2021-03-12 19:57:43 +0530

    Explanation of the functions

--- R/fct_function_file.R
+++ R/fct_function_file.R
@@ -1,6 +1,22 @@
 #' @import rlang
 
 
+## A filter function for enrollement tab
+# df is a dataframe 
+# col is column in the dataframe df
+# val is value to filter value
+
+# Exammple : 
+# df 
+#  ColA   ColB
+#  "AXS"   "SDF"
+#  "SDA"   "SDS"
+
+## filter_function(df, ColA, AXS)
+#   ColA   ColB
+#  "AXS"   "SDF"
+
+
 filter_funtion <- function(df, col, val){
   
   if(!is.null(val)){
